79208cf39a7768c8dfc27975a00d29d1223f2ecf
Alexander <Alexyammine94@gmail.com>
2020-07-14 09:01:22 -0400

    Added close() to types

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -10,4 +10,5 @@ export class Messenger {
     constructor(channelName: string, onMessage: onMessageCallback);
     sendMessage(winContext: Window, message: Object, origin: string): void;
     sendMessage(winContext: Window, message: Object, origin: string, options: SendMessageOptions): Promise<void>;
+    close(): void;
 }
