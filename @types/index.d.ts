75fb1d88a8490c7993e3ab453b1cb462e800b56d
Alexander <Alexyammine94@gmail.com>
2020-07-21 11:11:29 -0400

    Added messanger instance to onMessage callback

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -1,6 +1,6 @@
 type ReplyCB = (response: Object) => void;
 
-type onMessageCallback = (req: any, origin: string, source: Window, sendResponse: ReplyCB) => void;
+type onMessageCallback = (req: any, origin: string, source: Window, sendResponse: ReplyCB, bridge: Messenger) => void;
 
 interface SendMessageOptions {
     waitForReply: boolean;
