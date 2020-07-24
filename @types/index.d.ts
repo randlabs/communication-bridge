b0dc7f8fc316d4976f835f3a829e834398105c50
Alexander <Alexyammine94@gmail.com>
2020-07-24 13:21:08 -0400

    Added more types

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -3,12 +3,12 @@ type ReplyCB = (response: Object) => void;
 type onMessageCallback = (req: any, origin: string, source: Window, sendResponse: ReplyCB, bridge: Messenger) => void;
 
 interface SendMessageOptions {
-    waitForReply: boolean;
+    waitForReply?: boolean;
+    timeout?: number;
 }
 
 export default class Messenger {
     constructor(channelName: string, onMessage: onMessageCallback);
-    sendMessage(winContext: Window, message: Object, origin: string): void;
-    sendMessage(winContext: Window, message: Object, origin: string, options: SendMessageOptions): Promise<void>;
+    sendMessage(winContext: Window, message: Object, origin: string, options?: SendMessageOptions): Promise<void>;
     close(): void;
 }
